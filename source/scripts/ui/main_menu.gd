d0011ec5015acbca533f9867603bec1b2037d17f
Jan Mesarč <jame_581@windowslive.com>
2025-06-26 21:20:12 +0200

    Add main menu scene and UI script with button functionality

---
+++ source/scripts/ui/main_menu.gd
@@ -0,0 +1,31 @@
+extends Control
+
+# UI Components
+@onready var start_button: Button = $MarginContainer/VBoxContainer/HBoxContainer/RightVBoxContainer/MainMenuButtons/StartButton
+@onready var credit_button: Button = $MarginContainer/VBoxContainer/HBoxContainer/RightVBoxContainer/MainMenuButtons/CreditsButton
+@onready var exit_button: Button = $MarginContainer/VBoxContainer/HBoxContainer/RightVBoxContainer/MainMenuButtons/ExitButton
+
+
+func _ready() -> void:
+	# Connect button signals
+	start_button.pressed.connect(_on_start_button_pressed)
+	credit_button.pressed.connect(_on_credit_button_pressed)
+	exit_button.pressed.connect(_on_exit_button_pressed)
+
+	# Set the version text
+	var version_label: Label = $MarginContainer/VBoxContainer/HBoxContainer/RightVBoxContainer/VersionLabel
+	version_label.text = Global.get_game_version()
+
+
+func _on_start_button_pressed() -> void:
+	print("Start button pressed!")
+	SceneChanger.goto_scene(Constants.GameScenes.RACE)
+
+
+func _on_credit_button_pressed() -> void:
+	print("Credits button pressed!")
+	
+
+func _on_exit_button_pressed() -> void:
+	print("Exit button pressed!")
+	get_tree().quit()  # Exit the game
