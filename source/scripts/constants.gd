2b9d3cf5b723a436e5fa64b14312666849403b4b
Matej Gargula <gargula.matej@gmail.com>
2025-06-27 23:22:07 +0200

    Updated ui. Added icons. Added constant data for the animals

--- source/scripts/constants.gd
+++ source/scripts/constants.gd
@@ -14,6 +14,28 @@ const MAPS = {
 	GameScenes.RACE: "uid://cdbkg4q3myof1"
 }
 
+const ANIMAL_DATA = {
+	"DOG":    { "name": "Dog",     "icon": "uid://dhck1kstep5ij" },
+	"BEAVER": { "name": "Beaver",  "icon": "uid://ou0v5jgj6kxl" },
+	"GOAT":   { "name": "Goat",    "icon": "uid://df8nea6fxt3ot" },
+	"GIRAFFE":{ "name": "Giraffe", "icon": "uid://b0jwg2k4ghf0a" },
+	"KAKAPO": { "name": "Kakapo",  "icon": "uid://ptlloynhxaso" },
+	"RAT":    { "name": "Rat",     "icon": "uid://3ndqmve4wjom" },
+	"FISH":   { "name": "Fish",    "icon": "uid://cikat3coqaarj" },
+	"HIPPO":  { "name": "Hippo",   "icon": "uid://bjf7bro2bkwxg" },
+}
+
+enum AnimalId {
+	DOG,
+	BEAVER,
+	GOAT,
+	GIRAFFE,
+	KAKAPO,
+	RAT,
+	FISH,
+	HIPPO	
+}
+
 enum ObstacleEnum {
 	NONE,
 	SAND_CASTLE,
@@ -25,8 +47,8 @@ enum ObstacleEnum {
 }
 
 enum RaceStage {
-    UNKNOWN,
-    START,
-    MIDDLE,
-    FINISH
+	UNKNOWN,
+	START,
+	MIDDLE,
+	FINISH
 }
