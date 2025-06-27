0acbd763d4a0ae68ece802ec4d9c5c186f816cb0
Jan Mesarč <jame_581@windowslive.com>
2025-06-27 11:37:23 +0200

    Race level base preparation

--- source/scripts/autoloads/signal_bus.gd
+++ source/scripts/autoloads/signal_bus.gd
@@ -1,3 +1,10 @@
 extends Node
 
+# Autoload script for handling signals across the game
+
+# Game management signals
 signal map_loaded(map_name: Constants.GameScenes)
+
+# Race signals
+signal race_started()
+signal race_finished(winner: String)
