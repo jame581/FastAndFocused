2833ccfe534d9c18ac50ec09274b37b71964fa93
Emrysius <strislukyn@gmail.com>
2025-06-27 15:25:50 +0200

    add audio manager and audio files

---
+++ source/scripts/audio_manager.gd
@@ -0,0 +1,14 @@
+extends AudioStreamPlayer2D
+
+
+func _ready():
+	
+	SignalBus.race_started.connect(handle_race_started)
+	SignalBus.race_finished.connect(handle_race_finished)
+
+
+func handle_race_started() -> void:
+	$CrowdAudio.play()
+	
+func handle_race_finished(winner) -> void:
+	$CrowdAudio.stop()
