66764e38029b226207a9ffe48f2ad3b319db7b1f
Jan Mesarč <jame_581@windowslive.com>
2025-06-27 20:15:23 +0200

    Add beaver scene and implement animal animations for idle and movement states

--- source/scripts/game/animal.gd
+++ source/scripts/game/animal.gd
@@ -1,20 +1,25 @@
 extends CharacterBody2D
+
 class_name Animal
 
 @export_category("Animal Settings")
 @export var speed_array: Array[float] = [100.0, 150.0, 200.0]
+@export var obstacle_effect_dict = {}
+
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 # Local variables
 var current_speed : float = 0.0 
 var speed_index = 0
-
 var active_obstacle : Constants.ObstacleEnum = Constants.ObstacleEnum.NONE
 var obstacle_timeout : float = 0.0
-@export_category("Animal Settings")
-@export var obstacle_effect_dict = {}
+
 
 func _ready() -> void:
 	SignalBus.race_started.connect(handle_race_started)
+	animation_player.play("idle")
+
 
 func _physics_process(_delta: float) -> void:
 	if (obstacle_timeout < Time.get_ticks_msec() and obstacle_effect_dict.has(active_obstacle)):
@@ -29,6 +34,7 @@ func race_start() -> void:
 	print(name + "Race started")
 	speed_index = 0
 	current_speed = speed_array[speed_index]
+	animation_player.play("movement")
 
 
 func race_next_stage() -> void:
@@ -41,9 +47,12 @@ func race_end() -> void:
 	print(name + "Race ended")
 	current_speed = 0.0
 	SignalBus.animal_finished.emit(self)
+	animation_player.play("idle")
+
 
 func handle_race_started() -> void:
 	race_start()
+
 
 func affect_with_obstacle(obstacle: Constants.ObstacleEnum, duration: float) -> void:
 	active_obstacle = obstacle
