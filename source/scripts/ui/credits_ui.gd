549acbd48063f70855fbfec38eeb7413a3fa2411
Jan Mesarč <jame_581@windowslive.com>
2025-06-28 22:33:43 +0200

    Enhance focus navigation for UI elements and update credits panel behavior

--- source/scripts/ui/credits_ui.gd
+++ source/scripts/ui/credits_ui.gd
@@ -1,5 +1,8 @@
 extends MarginContainer
 
+@export var return_focus_on_close: Button
+
+@onready var close_button: Button = $BettingPanel/MarginContainer/root/Panel/MarginContainer3/MarginContainer3/Panel/CloseButton
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 var closing_ui: bool = false
@@ -14,11 +17,16 @@ func show_panel() -> void:
 	visible = true
 	animation_player.play("fade_in")
 	closing_ui = false
+	close_button.grab_focus()
 
 
 func hide_panel() -> void:
 	closing_ui = true
 	animation_player.play_backwards("fade_in")
+
+	if return_focus_on_close:
+		# If a button is specified, focus it when the panel closes
+		return_focus_on_close.grab_focus()
 
 
 func _on_animation_finished(_anim_name: String) -> void:
