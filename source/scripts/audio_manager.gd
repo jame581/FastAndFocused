757bdfb6bba25da215d11b88a53399d09e595dea
Emrysius <strislukyn@gmail.com>
2025-06-28 11:15:03 +0200

    add shouts

--- source/scripts/audio_manager.gd
+++ source/scripts/audio_manager.gd
@@ -6,7 +6,6 @@ func _ready():
 	SignalBus.race_finished.connect(handle_race_finished)
 	SignalBus.map_loaded.connect(handle_map_changed)
 	SignalBus.animal_triggered_obstacle.connect(handle_animal_trigger)
-#	SignalBus.game_over.connect(handle_gameover)
 	
 func handle_map_changed(mapEnum: Constants.GameScenes) -> void:
 	if mapEnum == Constants.GameScenes.MAIN_MENU:
@@ -52,10 +51,17 @@ func handle_animal_trigger (animalId: Constants.AnimalId, obstacle_enum: Constan
 	elif animalId == Constants.AnimalId.HIPPO:
 		$AnimalHippo.play()
 	
-	
-#func handle_game_over(mapEnum: Constants.GameScenes) -> void:
-#	if(mapEnum == Constants.GameScenes.GAME_OVER):
-#		$GameOver.play()
-#	else 
-#		$GameOver.stop()
+#func handle_shouts():
+#	randomize()
+
+#	$Button.pressed.connect(_on_Button_pressed)
+
+#func _on_Button_pressed(mapEnum: Constants.GameScenes):
+#	var shouts = [
+#		$Shout1,
+#		$Shout2
+#	]
+#
+#	var index = randi() % shouts.size()
+#	shouts[index].play()
 	
